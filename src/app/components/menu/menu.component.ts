642ca1cddeae92921b636de38ad09ac46648ca8e
juantorres550 <torresjuan550@gmail.com>
2021-02-22 15:28:50 -0500

    Updated

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  direccion = [
+      {
+        name: 'Meetings',
+        path:'/asesor/meetings',
+        icon:'video_call'
+      },
+      {
+        name: 'Calls',
+        path: '/asesor/calls',
+        icon: 'event_note'
+      }
+    ];
+    
   constructor() { }
 
   ngOnInit(): void {
